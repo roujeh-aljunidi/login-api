8d8f3ef52c3fb28f295aefd8ad2375283783abfc
roujeh-aljunidi <roujeh.aljunidi@hyperisland.se>
2026-03-05 12:19:09 +0100

    fix users route and get endpoint

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    const user = await user.find({}, {password: 0});
+    const users = await user.find({}, {password: 0});
     res.status(200).json(users);
 });
 
